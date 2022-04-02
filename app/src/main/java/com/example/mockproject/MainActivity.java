9115e7ea0aede752f583c788f9846820157152e1
krissmile31 <krissmile31@gmail.com>
2022-04-02 12:18:07 +0700

    fix fragment for bottom navigation bar

--- app/src/main/java/com/example/mockproject/MainActivity.java
+++ app/src/main/java/com/example/mockproject/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.mockproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +22,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigation);
-
         bottomNavigationView.setItemIconTintList(null);
+
+        replaceFragment(new HomeFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_nav:
+                        replaceFragment(new HomeFragment());
+                        return true;
+
+                    case R.id.music_nav:
+                        replaceFragment(new MusicFragment());
+                        return true;
+
+                    case R.id.setting_nav:
+                        replaceFragment(new SettingFragment());
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragment).commit();
     }
 }
